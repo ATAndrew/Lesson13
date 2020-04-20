d2fe3fea65e211091a1fc2c88801cc49b08237fd
Tirsu Andrew <tirsu.andrew@gmail.com>
2020-04-20 21:58:27 +0300

    JS

---
+++ main.js
@@ -0,0 +1,23 @@
+let userName = 'Andrei';
+console.log(typeof userName);
+
+let andrei = null;
+console.log(andrei)
+
+let yes = true;
+let no = false;
+console.log(typeof no);
+
+let x = 500;
+let y = 300;
+let z;
+
+console.log(z);
+
+z = x + y;
+
+console.log(typeof z);
+
+let person = {firstName:"Andrew", lastName:"Tirsu", age:32, eyeColor:"brown"}
+
+console.log(typeof person)
